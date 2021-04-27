3a49d2bb89bb0d8d6143f2793a90c1cb72523a81
수영 <@61.73.125.160>
2021-04-27 12:27:51 +0900

    2021-04-27 11:47 박수영 : 	-location_keyWorld : vo생성 	-location_sort :vo생성 	-mchoice : vo생성 	-notice : vo생성

---
+++ naduri/src/com/harang/naduri/jdbc/notice/model/vo/Notice.java
@@ -0,0 +1,68 @@
+package com.harang.naduri.jdbc.notice.model.vo;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Notice implements Serializable {
+
+	private static final long serialVersionUID = 1010L;
+	
+	private int n_no ;		// 글번호
+	private String n_title ;	// 제목
+	private String n_content ;	// 내용
+	private Date n_date ;       // 작성일 
+	
+	
+	public Notice() {}
+	
+	public Notice(int n_no, String n_title, String n_content, Date n_date) {
+		super();
+		this.n_no = n_no;
+		this.n_title = n_title;
+		this.n_content = n_content;
+		this.n_date = n_date;
+	}
+
+	@Override
+	public String toString() {
+		return "Notice [n_no=" + n_no + ", n_title=" + n_title + ", n_content=" + n_content + ", n_date=" + n_date
+				+ "]";
+	}
+
+	public int getN_no() {
+		return n_no;
+	}
+
+	public void setN_no(int n_no) {
+		this.n_no = n_no;
+	}
+
+	public String getN_title() {
+		return n_title;
+	}
+
+	public void setN_title(String n_title) {
+		this.n_title = n_title;
+	}
+
+	public String getN_content() {
+		return n_content;
+	}
+
+	public void setN_content(String n_content) {
+		this.n_content = n_content;
+	}
+
+	public Date getN_date() {
+		return n_date;
+	}
+
+	public void setN_date(Date n_date) {
+		this.n_date = n_date;
+	}
+	
+	
+	
+	
+}
+

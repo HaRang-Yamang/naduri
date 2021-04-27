8dd6e9f0f07eed2dc264d8bf437ea00564a0f466
Paris <Paris@DESKTOP-72D7A13>
2021-04-27 12:17:22 +0900

    2021-04-27 12:17 commit :
    	vo 수정
    	by hwangseok

---
+++ naduri/src/com/harang/naduri/jdbc/bookmark/model/vo/Bookmark.java
@@ -0,0 +1,81 @@
+package com.harang.naduri.jdbc.bookmark.model.vo;
+
+import java.io.Serializable;
+
+public class Bookmark implements Serializable {
+	
+	private static final long serialVersionUID = 1002L;
+	
+	private int mark_id;
+	private int l_no;
+	private int m_no;
+	private int rno;
+	
+	
+	public Bookmark() {	}
+
+
+	public Bookmark(int mark_id, int l_no, int m_no, int rno) {
+		super();
+		this.mark_id = mark_id;
+		this.l_no = l_no;
+		this.m_no = m_no;
+		this.rno = rno;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Bookmark [mark_id=" + mark_id + ", l_no=" + l_no + ", m_no=" + m_no + ", rno=" + rno + "]";
+	}
+
+
+	public int getMark_id() {
+		return mark_id;
+	}
+
+
+	public void setMark_id(int mark_id) {
+		this.mark_id = mark_id;
+	}
+
+
+	public int getL_no() {
+		return l_no;
+	}
+
+
+	public void setL_no(int l_no) {
+		this.l_no = l_no;
+	}
+
+
+	public int getM_no() {
+		return m_no;
+	}
+
+
+	public void setM_no(int m_no) {
+		this.m_no = m_no;
+	}
+
+
+	public int getRno() {
+		return rno;
+	}
+
+
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+	
+}
---
+++ naduri/src/com/harang/naduri/jdbc/location/controller/none.java
@@ -0,0 +1,5 @@
+package com.harang.naduri.jdbc.location.controller;
+
+public class none {
+  // 폴더 없어지지 말라고 만든거에요 지우고 쓰세요
+}

dd388d4c72a6f7898f06c43071a5066b62d5d66b
tony7 <tony7@LAPTOP-2G1GQ22C>
2021-04-29 12:47:37 +0900

    2021-04-29 12:47 commit 리뷰작성 vo,service,dao 연결완료

--- naduri/src/com/harang/naduri/jdbc/review/model/dao/ReviewDAO.java
+++ naduri/src/com/harang/naduri/jdbc/review/model/dao/ReviewDAO.java
@@ -1,5 +1,94 @@
 package com.harang.naduri.jdbc.review.model.dao;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import static com.harang.naduri.jdbc.common.JDBCTemplate.*;
+
+import com.harang.naduri.jdbc.attach.model.vo.Attach;
+import com.harang.naduri.jdbc.review.model.vo.Review;
+
 public class ReviewDAO {
+private Properties prop;
+
+public ReviewDAO() {
+	prop= new Properties();
+	String filePath = ReviewDAO.class
+	          .getResource("/config/review.properties")
+	          .getPath();
+	try {
+		prop.load(new FileReader(filePath));
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+	public int insertReview(Connection con, Review r) {
+	int result =0;
+	PreparedStatement ps = null;
+	String sql = prop.getProperty("insertReview");
+	try {
+		ps = con.prepareStatement(sql);
+		ps.setInt(1, r.getR_rank());
+		ps.setString(2, r.getR_title());
+		ps.setString(3, r.getR_content());
+		ps.setString(4,r.getR_period());
+		ps.setInt(5, r.getR_with());
+				result = ps.executeUpdate();
+				
+	} catch (SQLException e) {
+
+		e.printStackTrace();
+	}finally {
+		close(ps);
+	}
+	
+	
+		return result;
+	}
+	public int getCurrentRno(Connection con) {
+		int result = 0;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String sql = prop.getProperty("currentRno");
+		try {
+			ps=con.prepareStatement(sql);
+			
+			rs=ps.executeQuery();
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(ps);
+		}
+
+		return result;
+	}
+	public int insertAttach(Connection con, Attach attach) {
+		int result =0;
+		PreparedStatement ps = null;
+		String sql =prop.getProperty("insertAttach");
+		
+		try {
+			ps= con.prepareStatement(sql);
+			ps.setString(1, attach.getAttach_name());
+			
+		 result =ps.executeUpdate();
+					} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(ps);
+		}
+		return result;
+	}
 
 }

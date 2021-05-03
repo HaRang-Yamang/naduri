4143169f14530a3f36e1faf1a8704cb8793346f2
Paris <Paris@DESKTOP-72D7A13>
2021-05-04 03:40:03 +0900

    2021-05-04 03:40 commit :
    	sql_file에 db익스포트 파일 저장해 두었습니다.
    
    	by Hwangseok

--- naduri/sql_file/210504_0312_SEQUENCE.sql
+++ naduri/sql_file/210504_0312_SEQUENCE.sql
@@ -1,5 +1,5 @@
 --------------------------------------------------------
---  파일이 생성됨 - 화요일-4월-27-2021   
+--  파일이 생성됨 - 화요일-5월-04-2021   
 --------------------------------------------------------
 DROP SEQUENCE "NADURI"."SEQ_A_NO";
 DROP SEQUENCE "NADURI"."SEQ_L_NO";
@@ -8,17 +8,17 @@ DROP SEQUENCE "NADURI"."SEQ_N_NO";
 DROP SEQUENCE "NADURI"."SEQ_Q_NO";
 DROP SEQUENCE "NADURI"."SEQ_R_NO";
 DROP SEQUENCE "NADURI"."SEQ_REPLYNO";
-DROP SEQUENCE "NADURI"."SEQ_SPOT_ID";
+DROP SEQUENCE "NADURI"."SEQ_S_ID";
 --------------------------------------------------------
 --  DDL for Sequence SEQ_A_NO
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "NADURI"."SEQ_A_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "NADURI"."SEQ_A_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence SEQ_L_NO
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "NADURI"."SEQ_L_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "NADURI"."SEQ_L_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence SEQ_M_NO
 --------------------------------------------------------
@@ -28,7 +28,7 @@ DROP SEQUENCE "NADURI"."SEQ_SPOT_ID";
 --  DDL for Sequence SEQ_N_NO
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "NADURI"."SEQ_N_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "NADURI"."SEQ_N_NO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 501 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence SEQ_Q_NO
 --------------------------------------------------------
@@ -45,7 +45,7 @@ DROP SEQUENCE "NADURI"."SEQ_SPOT_ID";
 
    CREATE SEQUENCE  "NADURI"."SEQ_REPLYNO"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
---  DDL for Sequence SEQ_SPOT_ID
+--  DDL for Sequence SEQ_S_ID
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "NADURI"."SEQ_SPOT_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "NADURI"."SEQ_S_ID"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;

65abb8c7cc812596b765f2f816e50c573a6a457f
gstre <gstre@DESKTOP-UFVJ0E3>
2021-05-06 17:21:57 +0900

    2021-05-06 17:21 commit
    	로그인 문제 해결
    
    박세홍

--- naduri/src/com/harang/naduri/jdbc/member/controller/MemberIoginServlet.java
+++ naduri/src/com/harang/naduri/jdbc/member/controller/MemberIoginServlet.java
@@ -2,12 +2,10 @@ package com.harang.naduri.jdbc.member.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
